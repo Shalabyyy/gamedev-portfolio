8be0e154aeac0df5091a0e1a099846ff04199a12
Youssef Shalaby <youshalaby@gmail.com>
2024-02-15 13:33:54 +0200

    Add Content for 3 Projects

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -1,98 +1,98 @@
 import ProjectData from '@/data/ProjectData.ts'
 
 export default [
-    new ProjectData("project-1", "Optimistic Chubby", "img/projects/project-1-icon.png", 
-    `
+    new ProjectData("project-1", "Birdie", "img/BirdieGiff.gif",
+        `
     <div class="paragraph">
-     <strong>Optimistic Chubby</strong> is a thing of beauty that I am so proud of. It's available on Android because why not.
-     <br/>Image by <a href="https://www.pexels.com/fr-fr/@knownasovan" target="_blank">OVAN</a>.
+     <strong>Birdie</strong> a game that brings the classic Tap and Flap concept to a whole new level of excitement!.
     </div>
     <div class="paragraph center">
-        <iframe class="youtube" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
-    </div>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/-uprqoScRKg?si=d-4o3_mSezrGVjf-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+    </iframe>    </div>
     <div class="paragraph center">
-        <a href="https://play.google.com/store/apps/details?id=some.playstore.thing" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
+        <a href="https://play.google.com/store/apps/details?id=com.YoussefShalaby.Birdie" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
     </div>
 
     <div class="paragraph">
         Main features :
         <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
+        <li>🐦 Six Charming Characters</li>
+        <li>🗺️ Three Enchanting Maps</li>
+        <li>💥 Power-Ups</li>
+        <li>🌟 Interactive Environments</li>
+        <li>🎮 Addictive Gameplay</li>
         </ul>
     </div>
 
     <div class="paragraph center">
-        <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
-        <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
+        <img class="phone-screenshot" src="img/Birdie4.png" alt="Birdie Screenshot 1" />
+        <img class="phone-screenshot" src="img/Birdie5.png" alt="BirdieScreenshot" />
     </div>
     `, "#23bd69", true),
-    new ProjectData("project-2", "Singing Addict", "img/projects/project-2-icon.png", `
+    new ProjectData("project-2", "Animal Drag Racing", "img/AnimalGif.gif", `
     <div class="paragraph">
-        <strong>Optimistic Chubby Blog</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
-        <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@adonyi-gabor-604571">Adonyi Gábor</a>.
+        <strong>Animal Drag Racing</strong> is my first ever published game that I am very proud of.
     </div>
     <div class="paragraph center">
-        <iframe class="youtube" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
-    </div>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/3s1AzkwVkNQ?si=CJ-T8bW-u4XwHVww&amp;start=30" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>    </div>
 
     <div class="paragraph">
         Main features :
         <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
+        <li>🏁Adventurous race in the form of an animal</li>
+        <li>🎶Superb Soundtrack and Pacing</li>
+        <li>👉One Button Control</li>
+        <li>📈Player Progression System</li>
         </ul>
     </div>
 
-    <div class="paragraph">
-    <div class="notice">
-        Windows build available on <a href="https://some.where/nice" target="_blank">itch.io</a>.
-        Source code is available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
+    <div class="paragraph center">
+    <a href="https://play.google.com/store/apps/details?id=io.YoussefShalaby.WildAnimalRacing&pcampaignid=web_share" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
+    <a href="https://youssefshalaby.itch.io/wild-animal-racing" target="_blank"><img src="img/projects/itch-store-logo.png" alt="Play Store badge" /></a>
+
     </div>
-    </div>
+  
 
     <div class="paragraph center">
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
+    <img class="pc-screenshot" src="img/Animal1.png" alt="Animal Drag Racing Screenshot" />
+    <img class="pc-screenshot" src="img/Animal2.png" alt="Animal Drag Racing Screenshot" />
+    <img class="pc-screenshot" src="img/Animal3.png" alt="Animal Drag Racing Screenshot" />
+    <img class="pc-screenshot" src="img/Animal4.png" alt="Animal Drag Racing    Screenshot" />
     </div>
     `, "#5a78af"),
-    new ProjectData("project-3", "Drawing Overload", "img/projects/project-3-icon.png", `
+    new ProjectData("project-3", "The Private", "img/PrivateGif.gif", `
     <div class="paragraph">
-        <strong>Drawing Overload</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
-        <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@miphotography">Miesha Maiden</a>.
+        <strong>The Private</strong> My long term solo project, which is still a work in progress, I have been working on this game on/off for 3+ Years, 
+        With my development journey being documented on youtube.
+        This is the first game I ever write story and content for. I plan on releasing this game in early 2026.
     </div>
     <div class="paragraph center">
-        <iframe class="youtube" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
-    </div>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/y3EyBBlIdfg?si=ay3q8lXxvpxkUlMM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>    </div>
 
     <div class="paragraph">
         Main features :
         <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
+        <li>Dynamic Enemy AI</li>
+        <li>Fluid Spray-n-Pray System</li>
+        <li>Realstic Ballistic Effects</li>
+        <li>Retro Dialouge Systems/li>
+        <li>(Almost) Evreything is Interactable</li>
+        <li>Dynamic Inventory Managment Systems</li>
+        <li>Inspired by true events</li>
         </ul>
     </div>
 
     <div class="paragraph">
         <div class="notice">
-        Playable in the browser (WebGL) on <a href="https://some.where/nice" target="_blank">itch.io</a>.
-        Source code is available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
+        You can watch the full development journey on <a href="https://youtube.com/playlist?list=PLdLrXpAa6gESJaF9_2gbkyxLcPEb0LUzs&si=RySFL8qMC57Sv-77 target="_blank">my youtube channel</a>.
         </div>
     </div>
 
     <div class="paragraph center">
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
+        <img class="pc-screenshot" src="img/Private1.png" alt="Drawing Overload Screenshot" />
+        <img class="pc-screenshot" src="img/Private2.png" alt="Drawing Overload Screenshot" />
+        <img class="pc-screenshot" src="img/Private3.png" alt="Drawing Overload Screenshot" />
+        <img class="pc-screenshot" src="img/Private4.png" alt="Drawing Overload Screenshot" />
     </div>
     `, "#383838"),
     new ProjectData("project-4", "Eugeneable", "img/projects/project-4-icon.png", `
@@ -150,4 +150,5 @@ export default [
         <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Cloud Drew Land Screenshot" />
         <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Cloud Drew Land Screenshot" />
     </div>`, "#e48246")
+    
 ];
